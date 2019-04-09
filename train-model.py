549b4424cb9d19cd4285592dd40cf61a4c020617
Mathias Seuret <mathias@seuret.com>
2019-04-09 15:54:16 +0200

    Trained a new classifier. Accuracy of 77.6% on test data.

--- train-model.py
+++ train-model.py
@@ -1,6 +1,7 @@
 import os
 import torch
 from torch import nn
+from tqdm import tqdm
 from torch import optim
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
@@ -12,60 +13,63 @@ from ocrd_typegroups_classifier.typegroups_classifier import TypegroupsClassifie
 from ocrd_typegroups_classifier.data.qloss import QLoss
 
 # Loading and preparing the network
-vraec = vraec50(layer_size=128, output_channels=16)
+vraec = vraec50(layer_size=(96, 96, 96, 96), output_channels=11)
 for l in range(2, 6):
     vraec.set_variational(l, False)
 use_variational_layer = True
 
 # Some settings for the training
-learning_rate = 0.001
+learning_rate = 0.01
 weight_decay = 0.0001
 lr_decay = lambda epoch: 0.97 ** epoch
 reconstruction_loss = nn.MSELoss()
 classification_loss = nn.CrossEntropyLoss()
-optimizer = optim.SGD(vraec.select_parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=0.9)
+#optimizer = optim.SGD(vraec.select_parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=0.9)
+optimizer = optim.Adam(vraec.select_parameters(), lr=learning_rate, weight_decay=weight_decay)
 scheduler = LambdaLR(optimizer, lr_lambda=[lr_decay])
 
 # Creation of the typegroup classifier
 tgc = TypegroupsClassifier(
     {
-        'Antiqua':0,
-        'Bastarda':1,
-        'Fraktur':2,
-        'Griechisch':3,
-        'Hebräisch':4,
-        'Kursiv':5,
-        'Rotunda':6,
-        'Textura':7,
-        'Adornment':8,
-        'Book covers and other irrelevant data':9,
-        'Empty Pages':10,
-        'Woodcuts - Engravings':11
+        'andere_schrift': 0,
+        'antiqua': 1,
+        'bastarda': 2,
+        'fraktur': 3,
+        'griechisch_kursiv': 4,
+        'hebräisch': 5,
+        'kursiv': 6,
+        'nicht_schrift': 7,
+        'rotunda': 8,
+        'schwabacher': 9,
+        'textura': 10
     },
     vraec
 )
+
+if os.path.exists(os.path.join('ocrd_typegroups_classifier', 'models', 'classifier.tgc')):
+    tgc = TypegroupsClassifier.load(os.path.join('ocrd_typegroups_classifier', 'models', 'classifier.tgc'))
 
 # Data transformation & loading
 # Note that due to the rotation, having several sequential shearing
 # transforms sequentially is not the same as having only one with
 # a larger range.
 trans = transforms.Compose([
-    transforms.RandomAffine(4, shear=10),
-    transforms.RandomAffine(4, shear=10),
-    transforms.RandomAffine(4, shear=10),
-    transforms.RandomCrop(224),
+    transforms.RandomAffine(12, shear=5),
+    transforms.RandomResizedCrop(224, scale=(0.25, 1.0), ratio=(0.5, 1.5), interpolation=2),
+    transforms.RandomAffine(12, shear=5),
+    #transforms.RandomCrop(224),
     transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.1, hue=0.05),
     QLoss(min_q=5, max_q=80),
     transforms.ToTensor()
 ])
 #training = ImageFolder('/cluster/seuret/patches/all', transform=trans)
 # TODO : replace by correct path
-training = ImageFolder('../extracted/samples', transform=trans)
+training = ImageFolder('/disks/data1/seuret/ocrd/labelbox/training_data', transform=trans)
 training.target_transform = tgc.classMap.get_target_transform(training.class_to_idx)
 
 
 data_loader = torch.utils.data.DataLoader(training,
-                                          batch_size=64,
+                                          batch_size=36,
                                           shuffle=True,
                                           num_workers=4)
 
@@ -79,13 +83,13 @@ for epoch in range(200):
     lossSum = 0
     good = 0
     known = 0
-    for sample, label in data_loader:
+    for sample, label in tqdm(data_loader):
         # Move data to device
         sample = sample.to(tgc.dev)
         label  = label.to(tgc.dev)
         
         # Training the (variational) autoencoder
-        #tgc.network.set_variational(5, use_variational_layer)
+        #tgc.network.set_variational(5, True)
         #rl = tgc.network.finetune(sample, optimizer=optimizer, loss_function=reconstruction_loss)
         tgc.network.set_variational(5, False)
         
@@ -104,7 +108,6 @@ for epoch in range(200):
         _, p = torch.max(out, 1)
         good += torch.sum(p==label).item()
         known += len(label)
-        print(lossSum)
     print('Epoch %d, loss %.1f, %d/%d=%.1f%%' % (epoch, lossSum, good, known, (100.0*good)/known))
     tgc.save(os.path.join('ocrd_typegroups_classifier', 'models', 'classifier.tgc'))
 
