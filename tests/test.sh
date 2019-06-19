9901b0d731d3223dccb8171ed09bd10a09508620
Gerber, Mike <mike.gerber@sbb.spk-berlin.de>
2019-06-19 09:34:58 +0200

    Check classification result in "make test"
    Make tests/test.sh check the result. As the test itself is written in
    shell, just use shell ways to check it - instead of actually parsing the
    XML.
    
    The classification check also assumes that the classification results
    are sorted by descending probabilities, i.e. that the result with
    highest probability is listed first.
    
    Fixes #3.

--- tests/test.sh
+++ tests/test.sh
@@ -11,5 +11,15 @@ ocrd-typegroups-classifier \
     -g PHYS_0011 \
     -m mets.xml \
     -I DEFAULT \
-    -O "OCR-D-FONTIDENT" \
+    -O OCR-D-FONTIDENT \
     -p <(echo '{"network": "'"$net"'", "stride":143}')
+
+expected_fontfamily='fraktur'
+if ! egrep -q "<pc:TextStyle fontFamily=\"$expected_fontfamily.*" OCR-D-FONTIDENT/OCR-D-FONTIDENT_FILE_0010_DEFAULT; then
+    echo 'Bad classification result:'
+    grep '<pc:TextStyle fontFamily=' OCR-D-FONTIDENT/OCR-D-FONTIDENT_FILE_0010_DEFAULT
+    exit 1
+else
+    echo 'Good classification result:'
+    grep '<pc:TextStyle fontFamily=' OCR-D-FONTIDENT/OCR-D-FONTIDENT_FILE_0010_DEFAULT
+fi
