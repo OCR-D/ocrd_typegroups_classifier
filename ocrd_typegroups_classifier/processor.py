7ccd1b90f7effce61055e4a1092a211435da2cf7
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2020-12-10 01:03:42 +0100

    filter image features, annotate Metadata/PcGtsId, add docstring

--- ocrd_typegroups_classifier/processor.py
+++ ocrd_typegroups_classifier/processor.py
@@ -3,10 +3,9 @@ Wrap TypegroupsClassifier as an ocrd.Processor
 """
 
 from ocrd import Processor
-from ocrd_utils import getLogger, concat_padded, MIMETYPE_PAGE
+from ocrd_utils import getLogger, make_file_id, MIMETYPE_PAGE
 from ocrd_models.ocrd_page import (
     to_xml,
-
     TextStyleType
 )
 from ocrd_modelfactory import page_from_file
@@ -24,6 +23,18 @@ class TypegroupsClassifierProcessor(Processor):
         self.log = getLogger('ocrd_typegroups_classifier')
 
     def process(self):
+        """Classify historic script in pages and annotate as font style.
+
+        Open and deserialize PAGE input files and their respective images.
+        Then for each page, retrieve the raw image and feed it to the font
+        classifier. 
+
+        Post-process detections by filtering classes and thresholding scores.
+        Annotate the good predictions by name and score as a comma-separated
+        list under ``/PcGts/Page/TextStyle/@fontFamily``, if any.
+
+        Produce a new PAGE output file by serialising the resulting hierarchy.
+        """
         network_file = self.parameter['network']
         stride = self.parameter['stride']
         classifier = TypegroupsClassifier.load(network_file)
@@ -35,8 +46,14 @@ class TypegroupsClassifierProcessor(Processor):
             page_id = input_file.pageId or input_file.ID
             self.log.info('Processing: %d / %s', n, page_id)
             pcgts = page_from_file(self.workspace.download_file(input_file))
+            self.add_metadata(pcgts)
             page = pcgts.get_Page()
-            pil_image, _, image_info = self.workspace.image_from_page(page, page_id)
+            pil_image, _, image_info = self.workspace.image_from_page(
+                # prefer raw image (to meet expectation of the models, which
+                # have been trained on RGB images with both geometry and color
+                # transform random augmentation)
+                # maybe even: dewarped,deskewed ?
+                page, page_id, feature_filter='binarized,normalized,grayscale_normalized,despeckled')
             # todo: use image_info.resolution
             result = classifier.run(pil_image, stride)
             score_sum = 0
@@ -75,9 +92,8 @@ class TypegroupsClassifierProcessor(Processor):
                     textStyle = TextStyleType()
                     page.set_TextStyle(textStyle)
                 textStyle.set_fontFamily(output)
-                file_id = input_file.ID.replace(self.input_file_grp, self.output_file_grp)
-                if file_id == input_file.ID:
-                    file_id = concat_padded(self.output_file_grp, n)
+                file_id = make_file_id(input_file, self.output_file_grp)
+                pcgts.set_pcGtsId(file_id)
                 self.workspace.add_file(
                     ID=file_id,
                     file_grp=self.output_file_grp,
